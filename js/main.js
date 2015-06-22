cdac1fbdc5b021123a9efbccedcfdd3789543c38
kingspp <kingspprathyush@gmail.com>
2015-06-22 16:03:29 +0530

    Modified save function to send two parameters

--- js/main.js
+++ js/main.js
@@ -12,16 +12,18 @@ foo.appendChild(element);
 function save(){
 	var response=document.getElementById("response");
 	var data = 'data='+document.getElementById("data").value;
+	var head = 'head='+document.getElementById("head").value;	
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function(){
 	  if (xmlhttp.readyState==4 && xmlhttp.status==200){
-	    response.innerHTML='<a href="files/'+xmlhttp.responseText+'.txt">'+xmlhttp.responseText+'.txt</a>';
+	    response.innerHTML='<a href="/'+xmlhttp.responseText+'.txt">'+xmlhttp.responseText+'.txt</a>';
 	  }
 	}
 	xmlhttp.open("POST","write.php",true);
         //Must add this request header to XMLHttpRequest request for POST
         xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xmlhttp.send(data);
+	xmlhttp.send( "head= "+head+
+              "&data= "+data); 
 }
 
 function addElement() {
