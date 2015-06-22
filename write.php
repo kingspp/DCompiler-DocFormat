5654f748661be197b4a924a75352aac459acb013
kingspp <kingspprathyush@gmail.com>
2015-06-22 16:05:10 +0530

    Fixed issues with raw input data

--- write.php
+++ write.php
@@ -3,6 +3,8 @@
 //var_dump($_SERVER);
 $post_head = $_POST['head'];
 $post_data = $_POST['data'];
+$post_head=str_replace("head=","",$post_head);
+$post_data=str_replace("data=","",$post_data);
 if (!empty($post_data)) {
     //$dir = 'D:/';
     $file = uniqid().getmypid();
