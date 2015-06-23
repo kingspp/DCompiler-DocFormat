0668dde2136ad9cf5c92d6c00a50f53e78be841e
kingspp <kingspprathyush@gmail.com>
2015-06-23 23:28:55 +0530

    Fixed issue with del.php

--- php/del.php
+++ php/del.php
@@ -1,5 +1,6 @@
 <?php
-$files = glob('files/*.txt'); // get all file names
+	
+$files = glob('../files/*.txt'); // get all file names
 foreach($files as $file){ // iterate files
   if(is_file($file))
     unlink($file); // delete file
