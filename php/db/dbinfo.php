cbc6baee6a773b97d904e6040ed03b7073222ec3
kingspp <kingspprathyush@gmail.com>
2019-10-04 13:33:12 -0400

    Updated files

--- php/db/dbinfo.php
+++ php/db/dbinfo.php
@@ -1,7 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "documat";
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$username = "b5f5835a63f843";
+$password = "bdc69961";
+$dbname = "heroku_98ca76ab65893f1";
 $table = "documents";
 ?>
+
+
+<!-- mysql://b5f5835a63f843:bdc69961@us-cdbr-iron-east-05.cleardb.net/heroku_98ca76ab65893f1?reconnect=true -->
